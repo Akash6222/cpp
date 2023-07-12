092ceb0a6ca7e2202d2b51cedffe1b6bf0c27f63
Akash6222 <akashdubey25274@gmail.com>
2023-07-13 01:56:37 +0530

    inbtroduction

---
+++ 1_Introduction/1_Introduction.cpp
@@ -0,0 +1,7 @@
+#include<iostream>    //This is library contains cin and cout variable
+using namespace std;      // This means I want to use cin and cout of std
+
+int main(){
+    cout<< "Namaste Duniya" << endl;      // << means to write or display // endl :- next line (enter)
+                                          // ; means to end the line like full stop in sentence 
+}
