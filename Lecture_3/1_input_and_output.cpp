afccafb3c7fd225a841646cdb4626877325cc450
Akash6222 <akashdubey25274@gmail.com>
2023-07-16 02:33:33 +0530

    Updated 1_input_and_output

--- Lecture_3/1_input_and_output.cpp
+++ Lecture_3/1_input_and_output.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main() {
+/*
+// One Input
     int n;  
     cout << "Enter the value of n: ";
     cin >> n; // cin is used to take input
     cout <<"Value of n is "<< n<< endl;
+*/
+
+/*
+// Two inputs
+   int a,b;
+   cin>> a >> b;      // After giving 1st number enter then type 2nd number
+                      // cin doesn't read " ", "\t", "\n"
+
+   cout<<"Value of a and b is "<< a <<" "<< b <<endl;
+*/
+
+// cin.get
+   int a;
+   a = cin.get();           // through this we can take " ", "\t", "\n" and converted into numeric and then displayed
+                            // a assic value are also applicable
+   cout<<"Value of a is "<< a << endl;
 }
