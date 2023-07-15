2d6166435cfc3917c2d66574482d3e16c4caa57f
Akash6222 <akashdubey25274@gmail.com>
2023-07-15 19:00:20 +0530

    Updated 2_type_casting

--- 2_Data_Types_&_Variable/2_type_casting.cpp
+++ 2_Data_Types_&_Variable/2_type_casting.cpp
@@ -2,15 +2,34 @@
 using namespace std ;
 
 int main (){
-    char ch = 'a';               // a is mapped with 97 and b with 98 and so on and these numbers are converted into binary that's how char saves data
-    cout << ch <<endl;
+    char ch = 'a';   
+    cout << ch <<endl;    
+
     char ch1 = 'b'; 
     cout << ch1 <<endl;
 
-    // Type Casting
-    int ch2 = 'a';             // same as ch2 = 97
-    cout << "ch2 character is type casted to integer value of a = " << ch2 <<endl;
+// // Type Casting
 
-    int ch3 = 'b';             // same as ch3 = 98
-    cout << "ch3 character is type casted to integer value of b = " << ch3 <<endl;
+    // char to int
+    // a is mapped with 97 and b with 98 and so on and these numbers are converted into binary that's how char saves data
+   
+    int in = 'a';             // int doesn't saves char that's why when it replace that alphabet with it's int value
+    cout << "a char is type casted into integer value of " << in <<endl;     
+
+    int in1 = 'b';             // same as ch3 = 98
+    cout << "b char is type casted into integer value of " << in1 <<endl;
+
+    // int to char
+    char ch2 = 97;
+    cout << "97 int is type casted to char value of " << ch2 <<endl;
+
+    char ch3 = 98;
+    cout << "98 int is type casted to char value of " << ch3 <<endl;
+
+    // Doubt
+    char ch4 = 12345;  
+    cout << ch4 <<endl;   
+
+    char ch5 = 123456;
+    cout << ch5 <<endl;
 }
