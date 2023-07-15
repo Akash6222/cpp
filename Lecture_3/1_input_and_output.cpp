b70bf3ab065e434ba37bad62072aa095298724ad
Akash6222 <akashdubey25274@gmail.com>
2023-07-15 22:07:20 +0530

    added 1_input_and_output

---
+++ Lecture_3/1_input_and_output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n; // cin is used to take input
+    cout <<"Value of n is "<< n<< endl;
+}
