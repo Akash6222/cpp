78e75bef23fbf24b6544852fdfbdce9b44fce18c
Akash6222 <akashdubey25274@gmail.com>
2023-07-16 21:07:54 +0530

    added 2_fahrenheit_to_celcius_table

---
+++ Lecture_3/3_while_loop/2_questions/2_fahrenheit_to_celcius_table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float c,f;
+    cout<<"Enter value in fahrenheit is ";
+    cin>>f;
+
+    c = 5*(f-32)/9;
+    
+    cout<<"The value in celcius is "<<c<<endl;
+
+}
