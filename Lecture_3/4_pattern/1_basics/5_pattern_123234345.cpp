ee0a1c1651ec957273905acda78b478fffb7d660
Akash6222 <akashdubey25274@gmail.com>
2023-07-18 20:35:49 +0530

    added 5_pattern_123234345

---
+++ Lecture_3/4_pattern/1_basics/5_pattern_123234345.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    
+    int row = 1;
+    int count = 1;
+
+    while(row<=n){
+        int col = 1;
+        int currentCount = count + row - 1;
+        while(col<=n){
+            cout << currentCount << " ";
+            currentCount = currentCount + 1;
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+}
+
+// 1 2 3 4 
+// 5 6 7 8 
+// 9 10 11 12 
+// 13 14 15 16
