65746db08aee0fe11962c7c6d17a4e6b4be43b22
Akash6222 <akashdubey25274@gmail.com>
2023-07-18 21:13:55 +0530

    added 12_pattern_alphabet_ABBCCC

---
+++ Lecture_3/4_pattern/2_questions/12_pattern_alphabet_ABBCCC.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    int row = 1;
+    char start = 'A';
+
+    while (row <= n) {
+        int col = 1;
+        char currentChar = start + row - 1;
+
+        while (col <= row) {
+            cout << currentChar << " ";
+            currentChar = currentChar + 1;
+            col = col + 1;
+        }
+
+        cout << endl;
+        row = row + 1;
+    }
+    
+}
+
+// A 
+// BC 
+// CDE
