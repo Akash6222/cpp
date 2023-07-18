38f118f3523c32cae9411483b0826695c209acaf
Akash6222 <akashdubey25274@gmail.com>
2023-07-18 13:34:06 +0530

    added 7_patterN

---
+++ Lecture_3/4_pattern/1_basics/7_pattern_print_count_number_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    
+    int row = 1;
+
+    while(row<=n){
+        int col = 1;
+        int value =  row;  // we read row 
+
+        while(col<=row){
+            cout<<value;       // we print value not row and append it value
+            col = col+1;
+            value = value+1;
+        }
+        cout<<endl;
+        row = row+1;
+    }
+}
+
+// 1
+// 23
+// 345
+// 4567
