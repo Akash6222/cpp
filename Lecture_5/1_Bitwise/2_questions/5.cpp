cea1c489d38849b0c1a4944cdfdf90b0fa251c5b
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 00:42:39 +0530

    added 5

---
+++ Lecture_5/1_Bitwise/2_questions/5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 1;
+    int b = a++;
+    int c = ++a;
+
+    cout <<"The Value of b is "<< b <<endl ;
+    cout <<"The Value of c is "<< c <<endl ;
+
+}
