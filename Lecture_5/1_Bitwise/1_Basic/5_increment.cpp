c3b4f81428e3f60eba422e1b0edb396e6a0ba201
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 00:39:52 +0530

    added 5_increment

---
+++ Lecture_5/1_Bitwise/1_Basic/5_increment.cpp
@@ -0,0 +1,25 @@
+# include<iostream>
+using namespace std;
+
+// int main() {
+
+//     // post increment
+//     // first use then increase
+
+//     int i = 1;
+//     int a = 2;
+//     int sum = a + (i++);
+//     cout<<"Post incremented sum is "<<sum <<" and now i value is "<<i<<endl;
+
+// }
+
+int main(){
+
+    // pre increment
+    // first increase then use
+    int i = 1;
+    int a = 2;
+    int sum1 = a + (++i);
+    cout<<"Pre incremented sum1 is "<<sum1 <<" and now i value is "<<i<<endl;
+
+}
