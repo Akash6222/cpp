bc82c8cd5dd86bc4cc0a88bb1121cd8c69d4fc5c
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 00:38:54 +0530

    added 3_right_shift.cpp

---
+++ Lecture_5/1_Bitwise/1_Basic/3_right_shift.cpp
@@ -0,0 +1,13 @@
+// Right shift means shifting the bit to right
+// 5>>1 this means shifting all the bit to 1 right of it
+// 0000000....00101 after right shift 0000000....00010
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Right Shift of (17>>1) is "<< (17>>1)<<endl;
+    cout<<"Right Shift of (17>>2) is "<< (17>>2)<<endl;
+    cout<<"Right Shift of (19>>1) is "<< (19>>1)<<endl;
+    cout<<"Right Shift of (19>>2) is "<< (19>>2)<<endl;
+}
