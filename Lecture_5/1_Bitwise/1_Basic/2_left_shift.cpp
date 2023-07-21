2c6da11eb2309fe1a9b17a29c757e60f01ef54f1
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 00:38:23 +0530

    added 2_left_shift

---
+++ Lecture_5/1_Bitwise/1_Basic/2_left_shift.cpp
@@ -0,0 +1,13 @@
+// Left shift means shifting the bit to left 
+// 5<<1 this means shifting all the bit to 1 left of it
+// 0000000....00101 after left shift 0000000....01010
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Left Shift of (17<<1) is "<< (17<<1)<<endl;
+    cout<<"Left Shift of (17<<2) is "<< (17<<2)<<endl;
+    cout<<"Left Shift of (19<<1) is "<< (19<<1)<<endl;
+    cout<<"Left Shift of (19<<2) is "<< (19<<2)<<endl;
+}
