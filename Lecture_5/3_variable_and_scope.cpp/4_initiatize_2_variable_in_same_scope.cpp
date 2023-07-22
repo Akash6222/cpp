ef294c91d79d9a0e364a31de8052ff83b1208b0b
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 16:30:41 +0530

    added 4_initiatize_2_variable_in_same_scope

---
+++ Lecture_5/3_variable_and_scope.cpp/4_initiatize_2_variable_in_same_scope.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+
+    int b = 1;
+    cout<< b <<endl;
+
+    int b = 3;           // error: redeclaration of ‘int b
+    cout<< b <<endl;      
+
+}/
