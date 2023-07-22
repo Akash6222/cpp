3f67a81636f109aaf865b737133ff8f756edf4bd
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 16:54:05 +0530

    added 6_variable_in_block_under_a_block

---
+++ Lecture_5/3_variable_and_scope.cpp/6_variable_in_block_under_a_block.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+
+    if(1) {
+        int b;
+        if(1) {
+            int b=5;
+            if(1) {
+                int b=1;
+                cout<<b<<endl;
+            }
+        }
+    }    
+
+}
