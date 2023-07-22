3120132c124c9c428d70b9c833eef94732643800
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 16:46:45 +0530

    added 5_for_loop_scope

---
+++ Lecture_5/3_variable_and_scope.cpp/5_for_loop_scope.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+using namespace std;
+
+// int main() {
+
+//     int i = 8;
+
+//     for(; i<8; i++){         // int i variable outoff scope is used
+//         cout<<"Hi"<<endl;
+//     }    
+
+// }
+
+int main() {
+
+    int i = 5;
+
+    for(int i=0; i<5; i++){         // int i inscope is used
+        cout<<"Hi"<<endl;
+    }    
+
+}
