5067aff47ad0d757b5f52c9a6995e181d1e4c1e8
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 16:04:43 +0530

    updated 3_accessing_if_scope_variable_outside

--- Lecture_5/3_variable_and_scope.cpp/3_accessing_if_scope_variable_outside.cpp
+++ Lecture_5/3_variable_and_scope.cpp/3_accessing_if_scope_variable_outside.cpp
@@ -1,6 +1,20 @@
 # include<iostream>
 using namespace std;
 
+// int main() {
+
+//     int a = 3;
+//     cout<< a <<endl;
+
+//     if(true){
+//         int b = 5;
+//         cout<< b <<endl; 
+//     }
+
+//     cout<< b <<endl;     // b value can't be accessed 
+
+// }
+
 int main() {
 
     int a = 3;
@@ -11,6 +25,7 @@ int main() {
         cout<< b <<endl; 
     }
 
-    cout<< b <<endl;     // b value can't be accessed 
+    int b = 1;
+    cout<< b <<endl;   
 
 }
