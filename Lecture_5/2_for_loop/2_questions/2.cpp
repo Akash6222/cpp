31c0cd2e9d43f9ef8c7409dae5ad53b192cf1300
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 15:42:31 +0530

    added 2

---
+++ Lecture_5/2_for_loop/2_questions/2.cpp
@@ -0,0 +1,21 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+
+    for(int i=0; i<=5; i--){
+        cout<< i <<" "<<endl;
+        i++;
+    }
+}
+
+// 0 
+// 0 
+// 0 
+// 0 
+// 0 
+// 0 
+// 0 
+// 0 
+// 0 
+// 0 
