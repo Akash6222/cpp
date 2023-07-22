ef1f87d724ab9779bf2bc055ab74f117b37cff43
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 23:55:02 +0530

    added 1_Dec_To_Bin

---
+++ Lecture_6/1_Decimal_And_Binary/2_questions/1_negative_Dec_To_Bin.cpp
@@ -0,0 +1,32 @@
+# include<iostream>
+# include<math.h>          // for pow
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter the decimal value : ";
+    cin >> n;
+
+    if(n<0){
+        n = n*(-1);           // converted negative number to positive number
+    }
+
+    int ans = 0;
+    int i = 0;
+
+    while(n !=0 ){
+
+        int bit = n & 1;          // to get last bit
+
+        ans = (bit * pow(10,i) ) + ans;    // To get value in reverse
+
+        n = n>>1;                          // doing right shift so that last bit be removed and next bit will be used in while loop
+        i++;
+
+    }
+    cout<<"Answer is "<< ans <<endl;
+}
+
+
+// Note : We have stored the ans in int and int range is -2,147,483,648 to 2,147,483,647 if we bypass thois range then same input will be printed
