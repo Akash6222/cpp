787cdb1d9ce5c573d24405232a5c6db5c7bb717c
Akash6222 <akashdubey25274@gmail.com>
2023-07-22 22:55:16 +0530

    1_Dec_To_Bin

---
+++ Lecture_6/1_Decimal_And_Binary/1_Dec_To_Bin.cpp
@@ -0,0 +1,25 @@
+# include<iostream>
+# include<math.h>          // for pow
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter the decimal value : ";
+    cin >> n;
+
+    int ans = 0;
+    int i = 0;
+
+    while(n !=0 ){
+
+        int bit = n & 1;          // to get last bit
+
+        ans = (bit * pow(10,i) ) + ans;    // To get value in reverse
+
+        n = n>>1;                          // doing right shift so that last bit be removed and next bit will be used in while loop
+        i++;
+
+    }
+    cout<<"Answer is "<< ans <<endl;
+}
