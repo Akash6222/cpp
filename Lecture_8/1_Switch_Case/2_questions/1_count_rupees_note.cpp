5bf8c9c39c4a08353be569ae43555f76e4a9d4e8
Akash6222 <akashdubey25274@gmail.com>
2023-07-26 11:22:05 +0530

    updated 1_count_rupees_note

--- Lecture_8/1_Switch_Case/2_questions/1_count_rupees_note.cpp
+++ Lecture_8/1_Switch_Case/2_questions/1_count_rupees_note.cpp
@@ -9,63 +9,63 @@ int main() {
     cout << "Denominations:" << endl;
 
     // Calculate the number of notes for each denomination using a switch
-    switch (amount / 2000) {
+    switch ((amount / 2000) != 0) {
         case 1:
             cout << "Number of 2000 rupee notes: " << amount / 2000 << endl;
             amount %= 2000;
             break;
     }
 
-    switch (amount / 500) {
+    switch ((amount / 500) != 0) {
         case 1:
             cout << "Number of 500 rupee notes: " << amount / 500 << endl;
             amount %= 500;
             break;
     }
 
-    switch (amount / 100) {
+    switch ((amount / 100) != 0) {
         case 1:
             cout << "Number of 100 rupee notes: " << amount / 100 << endl;
             amount %= 100;
             break;
     }
 
-    switch (amount / 50) {
+    switch ((amount / 50) != 0) {
         case 1:
             cout << "Number of 50 rupee notes: " << amount / 50 << endl;
             amount %= 50;
             break;
     }
 
-    switch (amount / 20) {
+    switch ((amount / 20) != 0) {
         case 1:
             cout << "Number of 20 rupee notes: " << amount / 20 << endl;
             amount %= 20;
             break;
     }
 
-    switch (amount / 10) {
+    switch ((amount / 10) != 0) {
         case 1:
             cout << "Number of 10 rupee notes: " << amount / 10 << endl;
             amount %= 10;
             break;
     }
 
-    switch (amount / 5) {
+    switch ((amount / 5) != 0) {
         case 1:
             cout << "Number of 5 rupee notes: " << amount / 5 << endl;
             amount %= 5;
             break;
     }
 
-    switch (amount / 2) {
+    switch ((amount / 2) != 0) {
         case 1:
             cout << "Number of 2 rupee notes: " << amount / 2 << endl;
             amount %= 2;
             break;
     }
 
-    switch (amount / 1) {
+    switch ((amount / 1) != 0) {
         case 1:
             cout << "Number of 1 rupee notes: " << amount / 1 << endl;
             break;
