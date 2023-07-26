4f0541b7790aab1b9b41d2f875dabaa9847eaa35
Akash6222 <akashdubey25274@gmail.com>
2023-07-26 20:33:27 +0530

    modified  5_counting_function

--- Lecture_8/2_Functions/1_Basics/5_counting_function.cpp
+++ Lecture_8/2_Functions/1_Basics/5_counting_function.cpp
@@ -1,9 +1,10 @@
 # include <iostream>
 using namespace std;
 
-
+// Function signature (i.e it shows function returns what meaning whether it is int,bool,etc)
 // When functions doesn't return anything then use void
 void printCounting(int n){
+    // function body
     for( int i=1; i<=n; i++){
         cout<< i <<" ";
     }
@@ -16,6 +17,7 @@ int main(){
     cout << "Enter the number : ";
     cin >> n;
     
+    //function call
     printCounting(n);
 
     return 0;
