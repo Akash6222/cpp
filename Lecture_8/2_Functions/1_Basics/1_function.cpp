f292273d1b93ce9a25387889c3a63530c846b380
Akash6222 <akashdubey25274@gmail.com>
2023-07-26 18:44:05 +0530

    modified 1_function

--- Lecture_8/2_Functions/1_Basics/1_function.cpp
+++ Lecture_8/2_Functions/1_Basics/1_function.cpp
@@ -12,13 +12,22 @@ int power(int a, int b){
 }
 
 int main(){
-
+    
+    // 1st power
     int a,b;
     cout<<"Enter the number and it's power "; // 2 3 = 8
     cin>> a >> b;
     
     int answer = power(a,b);
     cout<<"Answer is "<< answer << endl;
+
+    // 2nd power
+    int c,d;
+    cout<<"Enter the number and it's power "; // 2 3 = 8
+    cin>> c >> d;
+    
+    int answer1 = power(c,d);
+    cout<<"Answer is "<< answer1 << endl;
 
     return 0;
 }
