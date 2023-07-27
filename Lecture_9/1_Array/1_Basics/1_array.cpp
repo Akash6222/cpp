2596548a8e9ade1b23edb4a6d62a45086868c4e9
Akash6222 <akashdubey25274@gmail.com>
2023-07-27 18:36:04 +0530

    added 1_array

---
+++ Lecture_9/1_Array/1_Basics/1_array.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+    int number[3] = {5, 6, 7};
+
+    cout << number[0] << endl;
+    cout << number[1] << endl;
+    cout << number[2] << endl;
+
+}
