38bd1b3e33ad1fa88e17941753d26900a92b1554
Akash6222 <akashdubey25274@gmail.com>
2023-07-27 22:07:33 +0530

    2_initialize_array

---
+++ Lecture_9/1_Array/1_Basics/2_initialize_0_array.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+    int number[10] = {0};    // Initializing the array with some values
+
+    // Printing all elements of the array
+    for (int i = 0; i < 10; i++) {
+        cout << number[i] << " ";
+    }
+
+    cout << endl; // Print a newline after printing the array elements
+
+    return 0;
+
+}
