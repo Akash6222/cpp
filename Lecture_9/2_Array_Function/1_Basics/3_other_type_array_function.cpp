25c60d022d9f5b80301c48cb69376c5feece2b55
Akash6222 <akashdubey25274@gmail.com>
2023-07-28 22:36:38 +0530

    modified 3_other_type_array_functio

--- Lecture_9/2_Array_Function/1_Basics/3_other_type_array_function.cpp
+++ Lecture_9/2_Array_Function/1_Basics/3_other_type_array_function.cpp
@@ -1,28 +1,29 @@
-# include <iostream>
+#include <iostream>
 using namespace std; 
 
 // function to print array
-void printArray( arr[], int size)       // bool arr, double arr,etc should work
+template<typename T>                 // this will detect the Datatype
+void printArray(T arr[], int size)   
 {
-    cout<< "Printing the whole array : ";
-    for(int i = 0; i<size; i++)
+    cout << "Printing the whole array: ";
+    for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
-    cout <<endl;
-    cout<< "Printing the Done: " <<endl;
+    cout << endl;
+    cout << "Printing done." << endl;
 }
 
 int main()
 {
     // declare array
     double first[3] = {1.23, 7.21};
-    //function call
-    printArray( first, 3);
+    // function call
+    printArray(first, 3);
 
     float second[3] = {1.1, 2.2, 3.8};
-    printArray( second, 3);
+    printArray(second, 3);
 
     bool third[3] = {0, 1, 0};
-    printArray( third, 3);
+    printArray(third, 3);
 }
