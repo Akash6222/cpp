6dd6e3b76e4a65a6fa28a26b17e7d85328ffb57e
Akash6222 <akashdubey25274@gmail.com>
2023-08-04 14:31:00 +0530

    added 2_optimized_linear_search

--- Lecture_12/1_Binary_Search/2_optimized_linear_search.cpp
+++ Lecture_12/1_Binary_Search/2_optimized_linear_search.cpp
@@ -6,7 +6,9 @@ int binarySearch(int arr[], int size, int key)
     int start = 0;
     int end  = size -1 ;
 
-    int mid = (start+end)/2;
+    // int mid = (start+end)/2;     // if start is 2^(-30) and end is 2^(-31) and mid can't store addition of it
+    
+    int mid = start + (end-start)/2;            // this inturn is (start+end)/2
 
     while (start <= end)
     {
@@ -24,7 +26,8 @@ int binarySearch(int arr[], int size, int key)
             end = mid - 1;
         }
 
-        mid = (start+end)/2;
+        // mid = (start+end)/2;
+        mid = start + (end-start)/2;
     }
     return -1;
 }
@@ -39,4 +42,5 @@ int main()
 
     cout << "Index of 4 is " << evenIndex <<endl;
     cout << "Index of 11 is " << oddIndex <<endl;
+
 }
