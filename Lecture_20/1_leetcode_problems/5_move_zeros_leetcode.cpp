0e39ca656f9a183e6c0abd167f1d698a9ef8c750
Akash6222 <akashdubey25274@gmail.com>
2023-12-14 14:24:31 +0530

    [ADD] move zero leetcode problem

---
+++ Lecture_20/1_leetcode_problems/5_move_zeros_leetcode.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        // Shift all non zero element to left
+        int nonZero = 0;
+
+        for(int j=0; j<nums.size(); j++){
+
+            if(nums[j] != 0){
+                swap(nums[j], nums[nonZero]);
+                nonZero++;
+            }
+        }
+    }
+};
+
+
+// Leetcode problem -> https://leetcode.com/problems/move-zeroes/
