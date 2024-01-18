015a3ed0c960235423385c53fbb48604880def63
Akash6222 <akashdubey25274@gmail.com>
2024-01-19 03:08:42 +0530

    Updated Lecture_4 5_pattern_123234345

--- Lecture_4/1_pattern/1_basics/5_pattern_123234345.cpp
+++ Lecture_4/1_pattern/1_basics/5_pattern_123234345.cpp
@@ -23,4 +23,23 @@ int main(){
 
 // 1 2 3 
 // 2 3 4 
-// 3 4 5 
+// 3 4 5
+
+
+// Another way
+    
+//    int i = 1;
+//   int count = 1;
+
+//    while(i<=n){
+//        int j = 1;
+//        while(j<=n){
+//            cout<<count<<" ";     
+//            count = count+1;
+//            j = j + 1;
+//        }
+//        count = count-2;          // this is what matter 
+//        cout<<endl;
+//        i = i+1;
+//    }
+
